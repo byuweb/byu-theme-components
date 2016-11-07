238e66bf883b5ed44c273a45f455692ba27a72ad
kebowdog <kevin.larry.bauer@gmail.com>
2016-11-07 16:40:56 -0700

    added menu demo
    Added working demo of top bar with menu (up to 6 links). Still need to
    configure for additional links.

---
+++ Scripts/Utilities.js
@@ -0,0 +1,22 @@
+﻿var utilities = {
+
+	loadTemplate: function (customElement, t) {
+		// load the template
+		let shadowRoot = customElement.attachShadow({ mode: 'open' });
+		const instance = t.content.cloneNode(true);
+		shadowRoot.appendChild(instance);
+	},
+
+	toggleClass: function(el, className, add) {
+	if (el) {
+		var classNames = el.className.split(/\s+/);
+		var index = classNames.indexOf(className);
+		if (add && index === -1) {
+			classNames.push(className);
+		} else if (!add && index !== -1) {
+			classNames.splice(index, 1);
+		}
+		el.className = classNames.join(' ');
+	}
+}
+}
