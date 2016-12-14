fbafefda1378f0a442b1f7bb95c0ac1db784a06e
kebowdog <kevin.larry.bauer@gmail.com>
2016-12-14 10:54:38 -0700

    Split into 3 files for script, template, styles

---
+++ components/byu-menu/script.js
@@ -0,0 +1,74 @@
+(function (doc) { // limit scope
+
+    class BYUMenu extends HTMLElement {
+
+        static get observedAttributes() {
+            return ['collapsed'];
+        }
+
+        get collapsed() {
+            return this.hasAttribute('collapsed');
+        }
+
+        set collapsed(val) {
+            if (val)
+                this.setAttribute('collapsed', '');
+            else
+                this.removeAttribute('collapsed');
+        }
+
+        constructor() {
+            super(); // always call super first
+            utilities.loadTemplate(this, doc.getElementById('ByuMenu'));
+        }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+            // We don't need to toggle a css class anymore. Using :host([collapsed]) instead
+            //utilities.toggleClass(this.shadowRoot.querySelector('.secondaryNav'), 'collapsed');                
+        }
+
+        connectedCallback() {
+            // if there are more than 6 links add the extras to a "more" dropdown
+            const slot = this.shadowRoot.querySelector("#slot");
+
+            var allLinks = slot.assignedNodes().filter(function (element) { return element instanceof HTMLElement });
+            
+            
+            // create the secondary nav links
+            for (var i = 0; i < allLinks.length; i++) {
+                var cln = allLinks[i].cloneNode(true);
+                this.shadowRoot.querySelector('.secondaryNav').appendChild(cln);
+            }
+
+            // calculate the height of the mobile dropdown
+            var h = allLinks.length * 48;
+            
+            if (allLinks.length > 6) {
+
+                // Since we want this one to show when there are 6 or less we need to manually switch it off instead
+                // of using nth-child like we do for the others 
+                allLinks[5].style.display = "none";
+
+                // create the "extra links" dropdown
+                var extraLinks = this.shadowRoot.querySelector('#extraLinks');
+                extraLinks.style.display = "table-cell";
+
+                allLinks = allLinks.slice(5);
+                var dropdown = extraLinks.querySelector("#extraLinksDropdown")
+                for (var i = 0; i < allLinks.length; i++) {
+                    var listItem = document.createElement("li");
+                    listItem.appendChild(allLinks[i]);
+                    dropdown.appendChild(listItem);
+                }
+            }
+
+            //dynamically set the height of the mobile dropdown based on the number of links
+            var styleSheet = this.shadowRoot.querySelector("#stylePlaceHolder");
+            styleSheet.innerHTML = "<style>.navbar-collapse { height: " + h + "px }</style>";
+        }
+    }
+
+
+    window.customElements.define('byu-menu', BYUMenu);
+
+})(document.currentScript.ownerDocument);
