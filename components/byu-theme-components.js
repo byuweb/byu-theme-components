35a44fd210320b79c9e7f65824e7cafc812940f8
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-05-19 17:41:30 -0600

    Rename script files

---
+++ components/byu-theme-components.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ThatJoeMoore on 2/18/17
+ */
+"use strict";
+
+/*
+ * Header Bits
+ */
+import './byu-header/byu-header.js';
+import './byu-menu/byu-menu.js';
+import './byu-search/byu-search.js';
+import './byu-user-info/byu-user-info.js';
+
+/*
+ * Footer Bits
+ */
+
+import './byu-footer/byu-footer.js';
+import './byu-footer-column/byu-footer-column.js';
+import './byu-footer-action-button/byu-footer-action-button.js';
+import './byu-social-media-links/byu-social-media-links.js';
+
