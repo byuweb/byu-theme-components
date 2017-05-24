0d1b26bccc22467af6170b52378d68acb5139699
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-05-24 08:18:06 -0600

    Externalize all images/icons (#197)
    * Replace embedded images with external ones.
    
    Moves the SVG icons and logos from being inlined to being served from
    the CDN, which can enhance our cacheability, as the images and icons
    aren't likely to change as often as the main components.
    
    Fixes #189. Fixes #187.
    
    * Replace inline more menu dropdown
    
    * Refactor byu-social-media-links.
    
    Moves everything into one slot, so that the consumer can choose the icon
    order. Now references external images instead of relying on fontawesome.
    
    Fixes #148. Fixes #181.
    
    * Move social styles to common stylesheet
    
    * Build distribution bundle

--- components/byu-social-media-links/byu-social-media-links.js
+++ components/byu-social-media-links/byu-social-media-links.js
@@ -1,6 +1,22 @@
 'use strict';
-import template from './byu-social-media-links.html';
+import template from "./byu-social-media-links.html";
 import * as util from "byu-web-component-utils";
+
+const SOCIAL_NAMES = {
+    'facebook': 'Facebook',
+    'twitter': 'Twitter',
+    'instagram': 'Instagram',
+    'youtube': 'YouTube',
+    'pinterest': 'Pinterest',
+    'google-plus': 'Google+',
+    'googleplus': 'Google+',
+    'gplus': 'Google+',
+    'linkedin': 'LinkedIn',
+    'rss': 'RSS',
+    'snapchat': 'Snapchat'
+};
+
+const SOCIAL_IDS = Object.getOwnPropertyNames(SOCIAL_NAMES);
 
 class BYUSocialMediaLinks extends HTMLElement {
 
@@ -11,22 +27,48 @@ class BYUSocialMediaLinks extends HTMLElement {
 
     connectedCallback() {
         util.applyTemplate(this, 'byu-social-media-links', template, () => {
-            var idArr = ['facebook', 'twitter', 'instagram', 'youtube', 'pinterest', 'google-plus', 'linkedin', 'rss', 'snapchat'];
-            var classArr = ['fa-facebook-official', 'fa-twitter', 'fa-instagram', 'fa-youtube-play', 'fa-pinterest', 'fa-google-plus-official', 'fa-linkedin', 'fa-rss-square', 'fa-snapchat'];
+            let main = this.shadowRoot.querySelector('#social-main');
+            applyTitleToChildren(main);
 
-            for (var i = 0; i < idArr.length; i++) {
-                const slotId = this.shadowRoot.querySelector("#" + idArr[i]);
-                var link = slotId.assignedNodes();
+            SOCIAL_IDS.forEach(id => {
+                const slot = this.shadowRoot.querySelector('#social-deprecated-' + id);
+                if (!slot) return;
 
-                if (link.length > 0) {
-                    link[0].classList.add("fa");
-                    link[0].classList.add(classArr[i]);
+                applyTitleToChildren(slot);
+                //We're still supporting the old way, but it's deprecated and people should move on.
+                if (slot.assignedNodes().length > 0) {
+                    console.log(`[WARNING] byu-social-media-links: deprecated usage of slot="${id}". Replace with class="${id}":`, this);
                 }
-            }
-
+            });
         });
     }
 }
 
 window.customElements.define('byu-social-media-links', BYUSocialMediaLinks);
 window.BYUSocialMediaLinks = BYUSocialMediaLinks;
+
+function applyTitleToChildren(slotElement) {
+    let kids = slotElement.assignedNodes();
+    kids.filter(k => k instanceof HTMLElement)
+    //Only grab the ones that don't already have a title
+        .filter(k => !k.title)
+        .forEach(kid => {
+            let kind = detectSocialKind(kid);
+            if (!kind) {
+                return;
+            }
+            //The spaces are a hack to avoid triggering adblockplus.
+            kid.title = ' ' + SOCIAL_NAMES[kind] + ' ';
+        });
+}
+
+function detectSocialKind(element) {
+    if (element.hasAttribute('slot')) return element.getAttribute('slot');
+    for (let i = 0; i < SOCIAL_IDS.length; i++) {
+        let id = SOCIAL_IDS[i];
+        if (element.classList.contains(id)) {
+            return id;
+        }
+    }
+    return null;
+}
