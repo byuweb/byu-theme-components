4c278abd86b4689db57bdd61c9ae1973ee32d2b7
Kevin Bauer <kevinlb@byu.local>
2017-06-09 14:19:11 -0600

    Modified load-component.js with ability to test other branches from demo site (not just the latest)

--- docs/load-components.js
+++ docs/load-components.js
@@ -28,10 +28,30 @@
 
 //Grab components JS and CSS. If running locally, grab it from /dist
 (function () {
-    var root = '//cdn.byu.edu/byu-theme-components/latest';
+    var root = '//cdn.byu.edu/byu-theme-components/';
     var min = true;
 
-    var isLocalhost = isLocalhost();
+
+    // allow user to set a version or branch in the querystring
+    var version = getParameterByName('version');
+    if (!version)
+    {
+        version = 'latest';
+    }
+    var branch = getParameterByName('branch');
+
+    if (branch)
+    {
+        version = 'experimental/' + branch;
+    }
+    root = root + version;
+
+    var isLocalhost = checkIfLocalhost();
+
+    // allow testing other branches from localhost
+    if (version != 'latest')
+        isLocalhost = false;
+
     var isForced = window.location.search && window.location.search.indexOf('load-local=true') >= 0;
 
     if (isLocalhost || isForced) {
@@ -61,10 +81,24 @@
         return u;
     }
 
-    function isLocalhost() {
+    function checkIfLocalhost() {
         var host = window.location.hostname;
         if (host === 'localhost') return true;
 
         return /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.test(host);
     }
+
+    // used to get values from the querystring, taken from an answer on StackOverflow.com
+    // https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+
 })();
