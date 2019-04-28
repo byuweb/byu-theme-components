1b2e8bda8f065589e803e0773accd131344c768f
Blake Morgan <blake_morgan@byu.edu>
2019-04-27 20:07:13 -0600

    Added seperate rollup for dev and produciton. Changed build folder to dist to match current spec.

--- rollup-dev.config.js
+++ rollup-dev.config.js
@@ -1,6 +1,8 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
 import multiEntry from 'rollup-plugin-multi-entry'
 import style from 'rollup-plugin-lit-html-style'
 
@@ -8,15 +10,17 @@ const esmodules = true;
 export default {
   // If using any exports from a symlinked project, uncomment the following:
   // preserveSymlinks: true,
-	input: ['components/**/*.js'],
-	output: {
-		file: 'dist/byu-theme-components.min.js',
+  input: ['components/**/*.js'],
+  output: {
+    file: 'dist/byu-theme-components.min.js',
     format: 'esm',
-		sourcemap: true
-	},
-	plugins: [
+    sourcemap: true
+  },
+  plugins: [
     resolve(),
     babel(),
+    serve('.'),
+    livereload(),
     multiEntry(),
     style({ esmodules }),
     minify({
