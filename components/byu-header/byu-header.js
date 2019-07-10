74dc65d22a544c0d89d73fabf78f53db5e190cc8
Blake Morgan <blake_morgan@byu.edu>
2019-07-09 20:59:33 -0600

    Inital header with single title.

--- components/byu-header/byu-header.js
+++ components/byu-header/byu-header.js
@@ -9,7 +9,18 @@ export class BYUHeader extends LitElement {
   render () {
     return html`
     <header class="byu-header">
-      
+      <div class="byu-header-logo">
+        <a href="https://www.byu.edu/" target="_blank">
+          <img src="https://cdn.byu.edu/shared-icons/latest/logos/monogram-white.svg" alt="BYU Logo" class="byu-logo" width="188" height="66">
+        </a>
+      </div>
+      <div class="byu-header-titles">
+        <div class="byu-header-title">
+          <a href="/" class="byu-site-title">
+            <slot name="site-title"></slot>
+          </a>
+        </div>
+      </div>
     </header>
     `
   }
