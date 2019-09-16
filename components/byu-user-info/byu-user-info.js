ca3ec4fe91fe8bffd3abc04664d54ac2379108fd
Blake Morgan <blake_morgan@byu.edu>
2019-09-16 08:53:44 -0600

    Add Joseph's tweaks.

--- components/byu-user-info/byu-user-info.js
+++ components/byu-user-info/byu-user-info.js
@@ -21,21 +21,13 @@ import style from './byu-user-info.sass'
 
 @customElement('byu-user-info')
 export class BYUUserInfo extends LitElement {
-  @property({ type: String, attribute: 'login-url' }) loginUrl = ''
+  // TODO: Make decision on login-url
+  // @property({ type: String, attribute: 'login-url' }) loginUrl = ''
 
   firstUpdated (_changedProperties) {
-    this._addSlotListeners()
     this._addAriaAttributes()
-
-    this.classList.add('byu-component-rendered')
-  }
-
-  _addSlotListeners () {
     this._setHasUser()
-    const userSlot = this.shadowRoot.querySelector('#user-name')
-    userSlot.addEventListener('slotchange', e => {
-      this._setHasUser()
-    })
+    this.classList.add('byu-component-rendered')
   }
 
   _setHasUser () {
@@ -61,7 +53,7 @@ export class BYUUserInfo extends LitElement {
   render () {
     return html`
 <div class="byu-id">
-    <slot name="user-name" id="user-name"></slot>
+    <slot name="user-name" id="user-name" @slotchange="_setHasUser()"></slot>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path fill="currentcolor" d="M50 95c-26 0-34-18-34-18 3-12 8-18 17-18 5 5 10 7 17 7s12-2 17-7c9 0 14 6 17 18 0 0-7 18-34 18z"/><circle cx="50" cy="50" r="45" fill="none" stroke="currentcolor" stroke-width="10"/><circle fill="currentcolor" cx="50" cy="40" r="20"/></svg>
     <slot name="logout" id="logout"></slot>
     <slot name="login" id="login"></slot>
