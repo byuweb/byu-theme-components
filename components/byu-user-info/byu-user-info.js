41d308bcef6cb29b7135461b64c35944507459d1
kebowdog <kevin.larry.bauer@gmail.com>
2020-01-27 15:45:27 -0700

    Moved slotchange event from template to javascript and corrected some css consistency errors.

--- components/byu-user-info/byu-user-info.js
+++ components/byu-user-info/byu-user-info.js
@@ -25,6 +25,9 @@ export class BYUUserInfo extends LitElement {
   // @property({ type: String, attribute: 'login-url' }) loginUrl = ''
 
   firstUpdated (_changedProperties) {
+    this.shadowRoot.querySelector('#user-name').addEventListener('slotchange', () => {
+      this._setHasUser();
+    });
     this._addAriaAttributes()
     this._setHasUser()
     this.classList.add('byu-component-rendered')
@@ -37,8 +40,12 @@ export class BYUUserInfo extends LitElement {
     if (userSlot.assignedNodes().length === 0 || userSlot.assignedNodes()[0].innerHTML === '') {
       userSlot.classList.add('hidden')
       logoutSlot.assignedNodes()[0].classList.add('hidden')
+      loginSlot.assignedNodes()[0].classList.remove('hidden')
     } else {
+      userSlot.classList.remove('hidden')
       loginSlot.assignedNodes()[0].classList.add('hidden')
+      logoutSlot.assignedNodes()[0].classList.remove('hidden')
+
     }
   }
 
@@ -53,7 +60,7 @@ export class BYUUserInfo extends LitElement {
   render () {
     return html`
 <div class="byu-id">
-    <slot name="user-name" id="user-name" @slotchange="_setHasUser()"></slot>
+    <slot name="user-name" id="user-name"></slot>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path fill="currentcolor" d="M50 95c-26 0-34-18-34-18 3-12 8-18 17-18 5 5 10 7 17 7s12-2 17-7c9 0 14 6 17 18 0 0-7 18-34 18z"/><circle cx="50" cy="50" r="45" fill="none" stroke="currentcolor" stroke-width="10"/><circle fill="currentcolor" cx="50" cy="40" r="20"/></svg>
     <slot name="logout" id="logout"></slot>
     <slot name="login" id="login"></slot>
